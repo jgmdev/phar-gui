3783a93f328af574fce428f1b28127aee4f6cef0
jgmdev <jgmdev@gmail.com>
2014-03-05 08:32:36 -0400

    Fix when adding a new file after selecting a file from tree.

--- lib/PharGUI/Window.php
+++ lib/PharGUI/Window.php
@@ -552,7 +552,9 @@ class Window extends PharFrameTemplate
                     $itemdata->fileinfo->getPath()
                 );
 
-                $current_item = $this->tree_ctrl->GetSelection();
+                $current_item = $this->tree_ctrl->GetItemParent(
+                    $this->tree_ctrl->GetSelection()
+                );
 
                 if(count($element) > 1)
                 {
